899e62489fd0934b63a7a7226480a98cd9d57241
RolandBarro <rolandbarro@gmail.com>
2020-05-11 07:25:50 +0800

    updated db config

--- backend/src/config/db.config.js
+++ backend/src/config/db.config.js
@@ -1,17 +1,17 @@
 require('dotenv').config();
-const { MONGO_ADMIN, DB_PWD } = process.env;
-
-console.log(MONGO_ADMIN, DB_PWD);
+const { MONGO_ADMIN, DB_PWD, NODE_ENV } = process.env;
 
 let uri, message;
 
-if(process.env.NODE_ENV === 'live') {
+if(NODE_ENV === 'live') {
   uri = `mongodb+srv://${MONGO_ADMIN}:${DB_PWD}@cluster0-tharj.mongodb.net/bios-ph?retryWrites=true&w=majority`;
   message = 'DB is connected at live mode.';
 } else {
   uri = 'mongodb://localhost/bios';
   message = 'DB is connected at local mode.';
 }
+
+console.log(uri, message);
 
 module.exports = {
   uri,
